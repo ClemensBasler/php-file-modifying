1493d813c1245a04832af605e0d8160c6db645bc
Clemens Basler <clemens-basler@t-online.de>
2017-11-16 14:38:34 +0100

    making it better

--- Class.php
+++ Class.php
@@ -1,13 +1,46 @@
 <?php
+/**
+ * @Entity @Table(name="weather_data")
+ **/
 
-class Test
+class WeatherData
 {
+  /**
+  * @Id @GeneratedValue @Column(type="integer")
+  * @var int
+  **/
   protected $id;
+/**
+* @Column(type="float")
+* @var float
+**/
 protected $wind_speed_ms
-;protected $wind_direction_deg
-;protected $sonic_temp_degC
-;protected $air_temp_degc
-;
+; 
+/**
+* @Column(type="float")
+* @var float
+**/
+protected $wind_direction_deg
+; 
+/**
+* @Column(type="float")
+* @var float
+**/
+protected $temp_sonic_degc
+; 
+/**
+* @Column(type="float")
+* @var float
+**/
+protected $air_temp_degc
+; 
+/**
+* @Column(type="float")
+* @var float
+**/
+protected $air_temp_ventilated_degc
+; 
+
 public function getId()
   {
       return $this->id;
--- write_file.php
+++ write_file.php
@@ -2,16 +2,20 @@
 $file = fopen("Class.php","w");
 $part1file = fopen ("part1.txt", "r");
 $part2file = fopen ("part2.txt", "r");
+$namingfile = fopen ("naming.txt", "r");
 $configFile = "config.txt";
 
 $part1 = fread($part1file,filesize("part1.txt"));
 $part2 = fread($part2file,filesize("part2.txt"));
+$naming = fread($namingfile,filesize("naming.txt"));
+
 $attributes = file($configFile);
 
 fwrite($file, $part1);
 
 for ($i=0; $i < sizeof($attributes); $i++) {
-  fwrite($file, "protected $$attributes[$i];");
+  fwrite($file, $naming);
+  fwrite($file, "protected $$attributes[$i]; \n");
 }
 fwrite($file, $part2);
 
